79609aeb0fccbe57309627bc451c2cdb21991f0d
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-09-30 10:00:16 +0200

    FR-9 - Styles added to Container

---
+++ src/components/container/Container.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
