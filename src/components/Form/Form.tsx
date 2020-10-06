b40a296610dc5abf0ce151313072689b120f4995
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-06 20:58:24 +0200

    ERSA-30 - Fixed form with flexbox

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,6 @@
 import {Field, Formik} from 'formik';
 import React from 'react';
+import {Chart} from '../chart/Chart';
 import {FieldElement} from './fieldElement/FieldElement';
 import {FormContainer, StyledForm, StyledInputWrapper} from './Form.styled';
 import {Values} from './Form.types';
@@ -19,35 +20,40 @@ export const FormComponent: React.FC<FormProps> = ({onSubmit}) => {
         >
             {({values}) => (
                 <StyledForm>
-                    <FormContainer>
-                        <h2>How much do you have currently saved?</h2>
-                        <StyledInputWrapper>
-                            <Slider />
-                            <Field name="initialValue" value={values.initialValue} component={FieldElement} unit="USD" />
-                        </StyledInputWrapper>
-                    </FormContainer>
-                    <FormContainer>
-                        <h2>How much do you want to save monthly?</h2>
-                        <StyledInputWrapper>
-                            <Slider />
-                            <Field name="monthlySaving" value={values.monthlySaving} component={FieldElement} unit="USD" />
-                        </StyledInputWrapper>
-                    </FormContainer>
-                    <FormContainer>
-                        <h2>How long do you plan to invest?</h2>
-                        <StyledInputWrapper>
-                            <Slider />
-                            <Field name="savingPeriod" value={values.savingPeriod} component={FieldElement} unit="Years" />
-                        </StyledInputWrapper>
-                    </FormContainer>
-                    <FormContainer>
-                        <h2>What is your expected annual profit?</h2>
-                        <StyledInputWrapper>
-                            <Slider />
-                            <Field name="annualProfit" value={values.annualProfit} component={FieldElement} unit="%" />
-                        </StyledInputWrapper>
-                    </FormContainer>
-                    <pre>{JSON.stringify(values, null, 2)}</pre>
+                    <div>
+                        <pre>{JSON.stringify(values, null, 2)}</pre>
+                        <Chart />
+                    </div>
+                    <div>
+                        <FormContainer>
+                            <h2>How much do you have currently saved?</h2>
+                            <StyledInputWrapper>
+                                <Slider />
+                                <Field name="initialValue" value={values.initialValue} component={FieldElement} unit="USD" />
+                            </StyledInputWrapper>
+                        </FormContainer>
+                        <FormContainer>
+                            <h2>How much do you want to save monthly?</h2>
+                            <StyledInputWrapper>
+                                <Slider />
+                                <Field name="monthlySaving" value={values.monthlySaving} component={FieldElement} unit="USD" />
+                            </StyledInputWrapper>
+                        </FormContainer>
+                        <FormContainer>
+                            <h2>How long do you plan to invest?</h2>
+                            <StyledInputWrapper>
+                                <Slider />
+                                <Field name="savingPeriod" value={values.savingPeriod} component={FieldElement} unit="Years" />
+                            </StyledInputWrapper>
+                        </FormContainer>
+                        <FormContainer>
+                            <h2>What is your expected annual profit?</h2>
+                            <StyledInputWrapper>
+                                <Slider />
+                                <Field name="annualProfit" value={values.annualProfit} component={FieldElement} unit="%" />
+                            </StyledInputWrapper>
+                        </FormContainer>
+                    </div>
                 </StyledForm>
             )}
         </Formik>
