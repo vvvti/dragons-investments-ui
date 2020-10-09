5ac3cd2db238a23f4929c510081186cc8a06377e
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-10-10 01:00:10 +0200

    ERSA-46 - Implement NavigationItems component
    create navigation items for better routing in application

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import dragonImage from '../../assets/dragon-eye.png';
 import {Logo, StyledNavbar, StyledTitle} from './Navbar.styled';
+import NavigationItems from './NavigationItems';
 
 export const Navbar = () => {
     return (
         <StyledNavbar>
             <StyledTitle>Dragons Investments</StyledTitle>
+            <NavigationItems />
             <Logo src={dragonImage} alt="Logo" />
         </StyledNavbar>
     );
---
+++ src/components/Navbar/NavigationItems.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledNavigationItems = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: hsl(230, 7%, 16%);
+    color: hsl(0, 0%, 100%);
+    width: 25rem;
+    text-decoration: none;
+`;
+export const StyledLink = styled.li`
+    text-decoration: none;
+`;
