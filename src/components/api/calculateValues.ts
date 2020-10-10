9ab8c3a21e9c344c1cc96990dab4bd926e614bca
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-10 13:21:31 +0200

    ERSA-48 - Add GET and POST method

--- src/components/api/calculateValues.ts
+++ src/components/api/calculateValues.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
+import {Values} from '../Form/Form.types';
 
 export const getData = async () => {
     try {
         const response = await axios.get(`/calculator`);
-        console.log(response.data);
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const postData = async (values: Values) => {
+    try {
+        const response = await axios.post(`/calculator`, {values});
+        console.log(response);
     } catch (error) {
         console.error(error);
     }
