af5e3a6c3e708b82e0bf0dd1fff156b2efaa15a6
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-10 14:42:04 +0200

    ERSA-49 - Small fixes on form and about component

--- src/components/SubPages/AboutPage.tsx
+++ src/components/SubPages/AboutPage.tsx
@@ -10,7 +10,7 @@ const AboutPage: React.FC = () => {
                 If You wonder which solution will be the best for you at Draco calculator, we have prepared a wide range of investment
                 calculators for you, thanks to which choosing the right path to multiply money will not be so difficult. Our investment
                 calculators are completely free applications that are updated regularly. This way you will get reliable results, enabling
-                you to choose the optimal produc
+                you to choose the optimal product.
             </StyledText>
         </StyledContainer>
     );
