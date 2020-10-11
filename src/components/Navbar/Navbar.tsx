a8fd65cd46145e924ca7c2694d64be36acf79b65
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-10-12 00:02:18 +0200

    ERSA-55 - Fix issues according to a code review

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import dragonImage from '../../assets/dragon-eye.png';
 import {Logo, StyledNavbar, StyledTitle} from './Navbar.styled';
-import NavigationItems from './NavigationItems';
+import NavbarItems from './NavbarItems';
 
 export const Navbar = () => {
     return (
         <StyledNavbar>
             <StyledTitle>Dragons Investments</StyledTitle>
-            <NavigationItems />
+            <NavbarItems />
             <Logo src={dragonImage} alt="Logo" />
         </StyledNavbar>
     );
