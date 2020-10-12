2894ba2365b2784c992af0a76c8233e91985eab7
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-12 10:21:32 +0200

    ERSA-59 - Contact details added

--- src/components/Navbar/NavbarItems/NavbarItems.styled.ts
+++ src/components/Navbar/NavbarItems/NavbarItems.styled.ts
@@ -1,6 +1,6 @@
 import {NavLink} from 'react-router-dom';
 import styled from 'styled-components';
-import {colors} from '../../styles/theme';
+import {colors} from '../../../styles/theme';
 const activeClassName = 'active';
 
 export const StyledNavigationItems = styled.ul`
--- src/components/SubPages/ContactPage.tsx
+++ src/components/SubPages/ContactPage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import man from '../../assets/man.png';
-import {StyledContainer, StyledTitle, StyledMan} from './SubPages.styled';
+import {StyledContainer, StyledTitle, StyledMan, ContactItem} from './SubPages.styled';
 
 const ContactPage: React.FC = () => {
     return (
         <StyledContainer>
             <StyledTitle>Contact Details</StyledTitle>
+            <ContactItem>
+                <h4>Contact Customer Support:</h4>
+                <p>fintech@challenge.com</p>
+                <h4>Talk to us:</h4>
+                <p>(+44) 7911-123-456</p>
+            </ContactItem>
             <StyledMan src={man} alt="Calculator" />
         </StyledContainer>
     );
