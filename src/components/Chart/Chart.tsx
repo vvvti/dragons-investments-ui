686c839e47847a2f260ba4166b70c205dbe534c5
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-13 14:25:36 +0200

    ERSA-66 - Add dynamic chart with props

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,42 +1,36 @@
 import {useFormikContext} from 'formik';
 import React from 'react';
-import {CartesianGrid, Line, LineChart, Tooltip, YAxis} from 'recharts';
+import {CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis} from 'recharts';
 import {Values} from '../Form/Form.types';
 import {ChartContainer} from './Chart.styled';
 
 export const Chart: React.FC = () => {
     const {
-        values: {initialValue, monthlySaving, savingPeriod, annualProfit, paymentFrequency},
+        values,
+        values: {monthlySaving},
     } = useFormikContext<Values>();
 
     //Random formulas for calculating start and end value
+    // label={{value: 'index', position: 'insideLeft', dy: 10}}
 
-    const data = [
-        {
-            name: '0',
-            deposit: initialValue,
-            profit: initialValue,
-            investmentValue: initialValue,
-        },
-        {
-            name: '1',
-            deposit: initialValue * annualProfit * paymentFrequency,
-            profit: monthlySaving * savingPeriod * 5,
-            investmentValue: initialValue * savingPeriod,
-        },
-        {
-            name: '2',
-            deposit: initialValue * annualProfit * paymentFrequency,
-            profit: monthlySaving * savingPeriod * 5,
-            investmentValue: initialValue * savingPeriod,
-        },
-        {
-            name: '3',
-            deposit: initialValue * annualProfit * paymentFrequency,
-            profit: monthlySaving * savingPeriod * 5,
-            investmentValue: initialValue * savingPeriod,
-        },
-    ];
+    const createArray = (values: Values) => {
+        var data = [];
+        var len = values.savingPeriod;
+        for (var i = 0; i < len; i++) {
+            data.push({
+                key: i,
+                deposit: values.monthlySaving * i * 10,
+                profit: values.paymentFrequency * monthlySaving * i,
+                investmentValue: values.initialValue * i,
+            });
+        }
+        return data;
+    };
+
+    const data = createArray(values);
+
+    console.log(data);
+
     return (
         <ChartContainer>
             <LineChart
@@ -45,13 +39,13 @@ export const Chart: React.FC = () => {
                 data={data}
                 margin={{
                     top: 5,
-                    right: 30,
+                    right: 100,
                     left: 20,
                     bottom: 5,
                 }}
             >
                 <CartesianGrid strokeDasharray="3 3" />
-                {/* <XAxis dataKey="name" /> */}
+                <XAxis dataKey="key" />
                 <YAxis />
                 <Tooltip />
                 <Line type="monotone" dataKey="deposit" stroke="#00e0b4" activeDot={{r: 8}} />
--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,6 @@
 export const CURRENCY: string = '£';
 export const INITIAL_AMOUNT: number = 2000;
 export const INITIAL_MONTHLY: number = 100;
-export const INITIAL_PERIOD: number = 2;
+export const INITIAL_PERIOD: number = 10;
 export const INITIAL_ANNUAL: number = 5;
 export const INITIAL_FREQUENCY: number = 1;
