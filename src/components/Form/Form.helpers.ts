2947653e4e5ce055f82d8ae93fa85eeb5c74fbe7
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-10-14 18:02:58 +0200

    ERSA-71 - Add new schemas to inputs and sliders validation

--- src/components/Form/Form.helpers.ts
+++ src/components/Form/Form.helpers.ts
@@ -4,18 +4,25 @@ export const validationSchema = yup.object({
     initialValue: yup
         .number()
         .typeError('Value must be a number')
-        .required('Field is required'),
+        .required('Field is required')
+        .positive('Must be a positive number')
+        .integer('Amount must be an integer'),
     monthlySaving: yup
         .number()
         .typeError('Value must be a number')
-        .required('Field is required'),
+        .required('Field is required')
+        .positive('Must be a positive number')
+        .integer('Amount must be an integer'),
     savingPeriod: yup
         .number()
         .typeError('Value must be a number')
         .max(50, 'You can select max 50 years')
-        .required('Field is required'),
+        .required('Field is required')
+        .positive('Must be a positive number')
+        .integer('Amount must be an integer'),
     annualProfit: yup
         .number()
         .typeError('Value must be a number')
-        .required('Field is required'),
+        .required('Field is required')
+        .positive('Must be a positive number'),
 });
