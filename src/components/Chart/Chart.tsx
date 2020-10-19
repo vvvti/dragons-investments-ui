74a510d1e52581e8abbccb5fc3036f366f73eb12
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-19 15:17:12 +0200

    ERSA-90 - Add currencyFormat function

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,6 +1,7 @@
 import {useFormikContext} from 'formik';
-import React from 'react';
+import React, {ReactText} from 'react';
 import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from 'recharts';
+import {currencyBasket} from '../../helpers/constants';
 import {FormValues, ResultsValue} from '../../helpers/types';
 import {ChartContainer} from './Chart.styled';
 
@@ -29,6 +30,15 @@ export const Chart: React.FC<ResultsValue> = ({...results}) => {
 
     const data = createArray(values, results, isValid);
 
+    const currencyFormat = (label: string | number | ReactText[]) => {
+        console.log(currencyValue);
+        if (currencyValue === currencyBasket.BRITISH_POUND) {
+            return `${currencyValue} ${label.toLocaleString('en-IN')}`;
+        } else {
+            return `${label.toLocaleString('en-IN')} ${currencyValue}`;
+        }
+    };
+
     return (
         <ChartContainer>
             <LineChart
@@ -46,7 +56,7 @@ export const Chart: React.FC<ResultsValue> = ({...results}) => {
                 <XAxis dataKey="key" />
                 <YAxis />
                 <Legend />
-                <Tooltip formatter={label => `${label.toLocaleString('en-IN')} ${currencyValue}`} />
+                <Tooltip formatter={label => currencyFormat(label)} />
                 <Line type="monotone" dataKey="deposit" stroke="#00e0b4" activeDot={{r: 8}} />
                 <Line type="monotone" dataKey="profit" stroke="#071e4a" />
                 <Line type="monotone" dataKey="investmentValue" stroke="#ff0000" />
