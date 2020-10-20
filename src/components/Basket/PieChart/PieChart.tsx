72001c179b0b3babed01c208101820b60a079212
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-20 18:21:03 +0200

    ERSA-114 - Added chart props

--- src/components/Basket/PieChart/PieChart.tsx
+++ src/components/Basket/PieChart/PieChart.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import {Cell, Pie, PieChart} from 'recharts';
+import {Cell, Legend, Pie, PieChart, Tooltip} from 'recharts';
 import {ChartContainer} from './PieChart.styled';
 
 export const PieChartComponent: React.FC = () => {
     const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
     const data = [
-        {name: 'Group A', value: 400},
-        {name: 'Group B', value: 300},
-        {name: 'Group C', value: 300},
-        {name: 'Group D', value: 200},
+        {name: 'Cash', value: 60},
+        {name: 'Bonds', value: 20},
+        {name: 'Stocks', value: 20},
     ];
 
     return (
         <ChartContainer>
-            <PieChart width={400} height={400}>
+            <PieChart width={500} height={500}>
                 <Pie
-                   data={data}
-                   cx={120}
-                   cy={200}
-                   innerRadius={60}
-                   outerRadius={80}
-                   fill="#8884d8"
-                   paddingAngle={5}
-                   dataKey="value"
+                    data={data}
+                    cx={250}
+                    cy={250}
+                    innerRadius={60}
+                    outerRadius={180}
+                    fill="#8884d8"
+                    paddingAngle={5}
+                    dataKey="value"
+                    label={true}
                 >
                     {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                     ))}
                 </Pie>
+                <Tooltip formatter={label => `${label}%`} />
+                <Legend />
             </PieChart>
         </ChartContainer>
     );
