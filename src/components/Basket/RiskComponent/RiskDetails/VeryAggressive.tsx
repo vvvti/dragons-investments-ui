ca982b483e564bfb0cb5fa71b806a500bc42aba4
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-20 12:40:10 +0200

    ERSA-111 - Created RiskDetails Component

---
+++ src/components/Basket/RiskComponent/RiskDetails/VeryAggressive.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const VeryAggressive: React.FC = () => {
+    return (
+        <div>
+            <h2>Very Aggressive</h2>
+            <h4>Typically, a very aggressive investor is:</h4>
+            <ul>
+                <li>primarily focused on pursuing above-average portfolio appreciation over time</li>
+                <li>someone who can tolerate higher degrees of fluctuation in the value of his investments</li>
+                <li>someone with high return expectations</li>
+                <li>someone who won’t need the money from their investments for 15 years or more</li>
+                <li>someone with a portfolio that has exposure to various asset classes but will be heavily invested in equities</li>
+            </ul>
+            <h4>At year 10, 3.7% of portfolios are losing money.</h4>
+            <p>Dragons Investments does not make recommendations on securities</p>
+        </div>
+    );
+};
