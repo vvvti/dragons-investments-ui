fbd8f24605f7cc92dafbcac7dc04eebd7c3d2875
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-21 14:20:22 +0200

    ERSA-118 - Create useBasket hook

--- src/components/Basket/Assumptiom/Assumption.styled.ts
+++ src/components/Basket/Assumptiom/Assumption.styled.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
+import {colors} from '../../../styles/theme';
 
 export const StyledAssumption = styled.div`
-    border-top: 2px solid grey;
+    border-top: 2px solid ${colors.basketBorder};
 `;
---
+++ src/components/Basket/BasketResults/BasketResults.styled.ts
@@ -0,0 +1,8 @@
+import NumberFormat from 'react-number-format';
+import styled from 'styled-components';
+
+export const StyledMainResult = styled(NumberFormat)`
+    font-size: 48px;
+    margin: 10px 0;
+    letter-spacing: 2px;
+`;
