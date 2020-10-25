6931cb9c7f61635513233696016a72dea7f5ed8e
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-25 14:35:03 +0100

    ERSA134 - Fixed styling

--- src/components/SubPages/OfferPage/OfferPage.styled.ts
+++ src/components/SubPages/OfferPage/OfferPage.styled.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-import {colors} from '../../../styles/theme';
 
 export const StyledOfferPage = styled.div`
-    border-top: 2px solid ${colors.basketBorder};
+    padding: 30px;
 `;
