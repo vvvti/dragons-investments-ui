f9737573db878f975b83a32ce170bb82ac1bb746
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-10-26 16:25:11 +0100

    ERSA-145 - Add dragon icons to all Offer components

--- src/components/SubPages/OfferPage/OfferFirst.tsx
+++ src/components/SubPages/OfferPage/OfferFirst.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import Zu from '../../../assets/ZU.jpg';
+import {OfferContainer, OfferImage} from './OfferPage.styled';
 
 export const OfferFirst: React.FC = () => {
     return (
-        <div>
-            <h2>Dragon Zu</h2>
-            <p>This basic package includes:</p>
-            <ul>
-                <li>
-                    <b>Insurance</b> - which covers one family member and one property
-                </li>
-                <li>
-                    <b>Investment</b> - development of a medium-term investment plan
-                </li>
-                <li>
-                    <b>Retirement</b> - plan your savings management so that you don't have to worry about your retirement in the future
-                </li>
-            </ul>
-        </div>
+        <OfferContainer>
+            <div>
+                <h2>Dragon Zu</h2>
+                <p>This basic package includes:</p>
+                <ul>
+                    <li>
+                        <b>Insurance</b> - which covers one family member and one property
+                    </li>
+                    <li>
+                        <b>Investment</b> - development of a medium-term investment plan
+                    </li>
+                    <li>
+                        <b>Retirement</b> - plan your savings management so that you don't have to worry about your retirement in the future
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <OfferImage src={Zu} alt="Dragon Zu" />
+            </div>
+        </OfferContainer>
     );
 };
--- src/components/SubPages/OfferPage/OfferSecond.tsx
+++ src/components/SubPages/OfferPage/OfferSecond.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import Griffin from '../../../assets/Griffin.jpg';
+import {OfferContainer, OfferImage} from './OfferPage.styled';
 
 export const OfferSecond = () => {
     return (
-        <div>
-            <h2>Dragon Griffin</h2>
-            <p>This standard package includes:</p>
-            <ul>
-                <li>
-                    <b>Insurance</b> - which covers two family members and one property
-                </li>
-                <li>
-                    <b>Investment</b> - development of a short- and medium-term investment plans for any purposes
-                </li>
-                <li>
-                    <b>Retirement</b> - plan your savings management so that you don't have to worry about your retirement in the future
-                </li>
-            </ul>
-            <p>Possibility of extension: Purchase of insurance for 2 movables</p>
-        </div>
+        <OfferContainer>
+            <div>
+                <h2>Dragon Griffin</h2>
+                <p>This standard package includes:</p>
+                <ul>
+                    <li>
+                        <b>Insurance</b> - which covers two family members and one property
+                    </li>
+                    <li>
+                        <b>Investment</b> - development of a short- and medium-term investment plans for any purposes
+                    </li>
+                    <li>
+                        <b>Retirement</b> - plan your savings management so that you don't have to worry about your retirement in the future
+                    </li>
+                </ul>
+                <p>Possibility of extension: Purchase of insurance for 2 movables</p>
+            </div>
+            <div>
+                <OfferImage src={Griffin} alt="Dragon Griffin" />
+            </div>
+        </OfferContainer>
     );
 };
