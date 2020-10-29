fc297e50083614faff0ad69cf403d5cc1361c60a
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-10-29 23:32:06 +0100

    ERSA-162 - Added test for postBasketResults

--- test/unit/api/basketApi.test.tsx
+++ test/unit/api/basketApi.test.tsx
@@ -3,7 +3,7 @@
  */
 import {axios} from '../../../src/api/rest/axios';
 import {mocked} from 'ts-jest/utils';
-import {postBasketResults} from '../../../src/api/basketApi';
+import {postBasketResults, putBasketResults} from '../../../src/api/basketApi';
 
 jest.mock('../../../src/api/rest/axios');
 
@@ -12,7 +12,7 @@ describe('basketApi', () => {
         mocked(axios.post).mockResolvedValue('getMock');
     });
 
-    it('calls request for basket and passes response', async () => {
+    it('post calls request for basket and passes response', async () => {
         mocked(axios.post).mockResolvedValue({data: 'getDataMock'});
         const data = {
             id: '',
@@ -24,5 +24,20 @@ describe('basketApi', () => {
         const request = await postBasketResults(data);
         expect(request.data).toEqual('getDataMock');
         expect(axios.post).toHaveBeenCalledWith('/basket', data);
+    });
+
+    it('put calls request for basket and passes response', async () => {
+        mocked(axios.put).mockResolvedValue({data: 'getDataMock'});
+        const data = {
+            id: '',
+            basketValue: 1000,
+            currency: 'GBP',
+            riskType: 2,
+        };
+        const id = '3fa85f64-5717-4562-b3fc-2c963f66afa6';
+
+        const request = await putBasketResults(id, data);
+        expect(request.data).toEqual('getDataMock');
+        expect(axios.put).toHaveBeenCalledWith(`/basket/${id}`, data);
     });
 });
